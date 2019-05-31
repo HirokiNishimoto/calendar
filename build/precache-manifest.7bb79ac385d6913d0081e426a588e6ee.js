a3297311e81522b186a46692ba1b86a26c32b27e
hirokinishimoto <shozemi.nishimoto@gmail.com>
2019-05-31 20:23:54 +0900

    ページ遷移とともに立体視画像が切り替わる仕様を追加

---
+++ build/precache-manifest.7bb79ac385d6913d0081e426a588e6ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15e07a5899da992a02d9478dbce59074",
+    "url": "/calender/index.html"
+  },
+  {
+    "revision": "4f345006d54acd760cd0",
+    "url": "/calender/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/calender/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "4f345006d54acd760cd0",
+    "url": "/calender/static/js/main.1ffd7170.chunk.js"
+  },
+  {
+    "revision": "b00c23bd4c9635b6c78e",
+    "url": "/calender/static/js/runtime~main.74e99931.js"
+  }
+]);
---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className='header'>
+        
+        <div className='header-title'>
+          <h2>視力回復 日めくりカレンダー</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
